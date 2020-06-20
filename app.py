f43faa678342dd470f6330c44d430d7b7961c5d8
Sri Velagapudi <srivar1997@gmail.com>
2020-06-19 23:18:26 -0400

    added in package for geoJSON conversion

--- app.py
+++ app.py
@@ -9,7 +9,14 @@ Author: Sri Velagapudi <sri.velagapudi@outlook.com>
 Date Created: 6/19/2020
 
 """
+import polyline
 
-#Convert PolyLine to GeoJSON
-def decode_polyline(string):
-    
+#Read from file and convert Polyline to GeoJSON
+with open('automatic-sample.csv','r') as file:
+    line = file.read()
+    word=line.split(",")
+    wordlen=len(word)
+    poly=(word[wordlen-2])
+    geo = polyline.decode(poly)
+    print(geo)
+
