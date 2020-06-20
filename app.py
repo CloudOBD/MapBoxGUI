5aa0649ebec4fe62dc4aa953ef178959ed9ab323
Sri Velagapudi <srivar1997@gmail.com>
2020-06-19 22:23:53 -0400

    created initial file

---
+++ app.py
@@ -0,0 +1,15 @@
+"""
+TripViewer version 1.0.0
+
+A Web Application to render CSV Data into a MapBox GUI
+
+File name: app.py
+Released under MIT License
+Author: Sri Velagapudi <sri.velagapudi@outlook.com>
+Date Created: 6/19/2020
+
+"""
+
+#Convert PolyLine to GeoJSON
+def decode_polyline(string):
+    
